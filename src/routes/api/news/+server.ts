94d3d05d6a921df114a6e74fa35bfc35892f5dd7
realfakenerd <lucas210898@hotmail.com>
2022-10-03 18:08:39 -0300

    fixed all news api

--- src/routes/api/news/+server.ts
+++ src/routes/api/news/+server.ts
@@ -1,26 +1,25 @@
 import { sources } from '$lib/sources';
 import axios from 'axios';
 import { load } from 'cheerio';
-import type { RequestHandler, RequestEvent } from './$types';
+
 const articles: any[] = [];
 
-sources.forEach(async (src) => {
-	const res = await axios.get(src.address);
-	const html = await res.data;
+export async function GET() {
+	sources.forEach(async (src) => {
+		const res = await axios.get(src.address);
+		const html = await res.data;
 
-	const $ = load(html);
-	$('a:contains("games")', html).each(function () {
-		const title = $(this).text();
-		const url = $(this).attr('href');
-		articles.push({
-			title,
-			url: src.base ? src.address + url : url,
-			sources: src.name
+		const $ = load(html);
+		$('a:contains("games")', html).each(function () {
+			const title = $(this).text();
+			const url = $(this).attr('href');
+			articles.push({
+				title,
+				url: src.base ? src.address + url : url,
+				sources: src.name
+			});
 		});
 	});
-});
-
-export async function GET({ request }: RequestEvent) {
 	const response = new Response(JSON.stringify(articles));
 
 	return response;
